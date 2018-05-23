1ddd5ed3c935a432abeaf788f543a89fd1223fdf
KimJungChul <kjc5443@gmail.com>
2018-05-23 13:16:48 +0900

    Adding put-item.sh

---
+++ aws-mongo-db/put-item/put-item.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+aws dynamodb put-item --table-name product --item file://./item2.json
+aws dynamodb put-item --table-name product --item file://./item3.json
+aws dynamodb put-item --table-name product --item file://./item4.json
