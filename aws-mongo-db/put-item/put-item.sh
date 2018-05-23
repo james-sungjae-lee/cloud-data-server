b29111ad341f406e013e576cfc1f5b636f839109
KimJungChul <kjc5443@gmail.com>
2018-05-23 13:44:42 +0900

    update put-item.sh

--- aws-mongo-db/put-item/put-item.sh
+++ aws-mongo-db/put-item/put-item.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 aws dynamodb put-item --table-name product --item file://./item2.json
+echo "dynamodb put-item item2.json"
 aws dynamodb put-item --table-name product --item file://./item3.json
+echo "dynamodb put-item item3.json"
 aws dynamodb put-item --table-name product --item file://./item4.json
+echo "dynamodb put-item item4.json"
