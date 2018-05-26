91e58487af6d9a6aea62d2ffe42bf470d62f9b33
KimJungChul <kjc5443@gmail.com>
2018-05-26 17:35:45 +0900

    Rename folder

---
+++ aws-dynamodb/put-item/get-item.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -o item2.json https://raw.githubusercontent.com/kmu-bigdata/cloud-data-server/master/aws-mongo-db/put-item/item2.json
+curl -o item3.json https://raw.githubusercontent.com/kmu-bigdata/cloud-data-server/master/aws-mongo-db/put-item/item3.json
+curl -o item4.json https://raw.githubusercontent.com/kmu-bigdata/cloud-data-server/master/aws-mongo-db/put-item/item4.json
