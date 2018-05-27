d58c4537434773e40dec8a1fe796a57b2409cc00
Kyungyong Lee <leeky@kookmin.ac.kr>
2018-05-27 15:01:00 +0900

    improve styling

--- wordcount/extractGenreFromMovies/WordCount.java
+++ wordcount/extractGenreFromMovies/WordCount.java
@@ -18,9 +18,11 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 public class WordCount {
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-	// Create a new job
+
+	      // Create a new job
         Job job = Job.getInstance(conf, "wordcount");
-	// Use the WordCount.class file to point to the job jar
+
+	      // Use the WordCount.class file to point to the job jar
         job.setJarByClass(WordCount.class);
 
         job.setOutputKeyClass(Text.class);
@@ -31,10 +33,12 @@ public class WordCount {
 
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
-	// Setting the input and output locations
+
+	      // Setting the input and output locations
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
-	// Submit the job and wait for it's completion
+
+        // Submit the job and wait for it's completion
         job.waitForCompletion(true);
     }
 
@@ -42,13 +46,15 @@ public class WordCount {
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
 
-        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        public void map(LongWritable key, Text value, Context context) 
+                throws IOException, InterruptedException {
             String line = value.toString();
-            String[] lineArray = line.split(",");
-            String lastWord = lineArray[lineArray.length - 1];
-            String[] genreArray = lastWord.split("\\|");
-            for (String genre_value : genreArray) {
-                word.set(genre_value);
+            String[] fields = line.split(",");
+            String lastField = fields[lineArray.length - 1];
+            String[] genres = lastField.split("\\|");
+
+            for (String genre : genres) {
+                word.set(genre);
                 context.write(word, one);
             }
         }
@@ -58,7 +64,8 @@ public class WordCount {
         public void reduce(Text key, Iterable<IntWritable> values, Context context)
                 throws IOException, InterruptedException {
             int sum = 0;
-	    // Sum all the occureeences of the word (key)
+            
+            // Sum all the occureeences of the word (key)
             for (IntWritable val : values) {
                 sum += val.get();
             }
