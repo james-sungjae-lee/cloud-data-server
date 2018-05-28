37e220f0f69a7003965a3917d0a6d03ac5f863d2
Kyungyong Lee <leeky@kookmin.ac.kr>
2018-05-28 22:28:54 +0900

    remove unnessary Decimal handler class

--- aws-dynamodb/lambda/queryByAsin.py
+++ aws-dynamodb/lambda/queryByAsin.py
@@ -4,16 +4,6 @@ import decimal
 
 from boto3.dynamodb.conditions import Key, Attr
 
-# Helper class to convert a DynamoDB item to JSON.
-class DecimalEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, decimal.Decimal):
-            if o % 1 > 0:
-                return float(o)
-            else:
-                return int(o)
-        return super(DecimalEncoder, self).default(o)
-
 def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
     table = dynamodb.Table('amazon-product-electronics')
