08daaf91e1d4bcd12095d8807527a1238b54a97c
KimJungChul <kjc5443@gmail.com>
2018-05-28 17:20:42 +0900

    update put-item.sh

--- aws-dynamodb/put-item/put-item.sh
+++ aws-dynamodb/put-item/put-item.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-aws dynamodb put-item --table-name product --item file://./item2.json
+
+aws dynamodb put-item --table-name amazon-product-electronics --item file://item2.json
 echo "dynamodb put-item item2.json"
-aws dynamodb put-item --table-name product --item file://./item3.json
+
+aws dynamodb put-item --table-name amazon-product-electronics --item file://item3.json
 echo "dynamodb put-item item3.json"
-aws dynamodb put-item --table-name product --item file://./item4.json
+
+aws dynamodb put-item --table-name amazon-product-electronics --item file://item4.json
 echo "dynamodb put-item item4.json"
