2f64090cf974cecbd2ab8dc5f2861c79e1713650
Kyungyong Lee <leeky@kookmin.ac.kr>
2018-05-29 13:48:15 +0900

    add space after a code

--- aws-lambda/Simple-Hello.py
+++ aws-lambda/Simple-Hello.py
@@ -6,5 +6,5 @@ def lambda_handler(event, context):
     print "Request ID:",context.aws_request_id
     print "Mem. limits(MB):", context.memory_limit_in_mb
     print "Time remaining (MS):", context.get_remaining_time_in_millis()
-    return 'Hello from '+event["name"]
+    return 'Hello from ' + event["name"]
 
