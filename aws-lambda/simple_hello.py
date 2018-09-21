9a69f4086d47e09b5f41843d0e023e76281f1c32
KimJungChul <kjc5443@gmail.com>
2018-09-21 16:27:26 +0900

    change file name

---
+++ aws-lambda/simple_hello.py
@@ -0,0 +1,10 @@
+def lambda_handler(event, context):
+    print "Received event:", event
+    print "Hello ", event["name"]
+    print "Log stream name:", context.log_stream_name
+    print "Log group name:",  context.log_group_name
+    print "Request ID:",context.aws_request_id
+    print "Mem. limits(MB):", context.memory_limit_in_mb
+    print "Time remaining (MS):", context.get_remaining_time_in_millis()
+    return 'Hello from ' + event["name"]
+
