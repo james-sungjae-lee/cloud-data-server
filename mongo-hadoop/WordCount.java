6f6e3f1dbdccc1e7e91fcfcd26cb9f99484af1ee
KimJungChul <kjc5443@gmail.com>
2018-10-11 09:38:27 +0900

    update indentation and code refactoring

--- mongo-hadoop/WordCount.java
+++ mongo-hadoop/WordCount.java
@@ -13,75 +13,72 @@ import org.bson.BSONObject;
 import org.bson.BasicBSONObject;
 
 public class WordCount {
+    public static void main(String[] args) throws Exception {
 
-	public static class Map extends Mapper<Object, BSONObject, Text, Text> {
+        Configuration conf = new Configuration();
+        MongoConfigUtil.setInputURI(conf, "mongodb://" + args[0]);
+        MongoConfigUtil.setOutputURI(conf, "mongodb://" + args[1]);
+        System.out.println("Conf: " + conf);
 
-		private final Text titleText = new Text();
-		private final Text genreText = new Text();
+        Job job = new Job(conf, "word count");
 
-		public void map(Object key, BSONObject value, Context context) throws IOException, InterruptedException {
+        job.setJarByClass(WordCount.class);
 
-			String title = value.get("title").toString();
-			String genre = value.get("genres").toString().replace("\\|", ",");
-			if (title.startsWith("a")) {
-				titleText.set(title);
-				genreText.set(genre);
-				context.write(titleText, genreText);
-			}
-		}
-	}
+        job.setMapperClass(Map.class);
 
-	public static class Combine extends Reducer<Text,Text,Text,Text>{
-		private final Text CombinerText = new Text();
+        job.setCombinerClass(Combine.class);
+        job.setReducerClass(Reduce.class);
 
-		public void reduce(Text key, Iterable<Text> values, Context context) 
-				throws IOException, InterruptedException {
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
 
-			CombinerText.set(values.iterator().next().toString());
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(BSONWritable.class);
 
-			context.write(key, CombinerText);
-		}
+        job.setInputFormatClass(MongoInputFormat.class);
+        job.setOutputFormatClass(MongoOutputFormat.class);
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+    public static class Map extends Mapper<Object, BSONObject, Text, Text> {
+        private final Text titleText = new Text();
+        private final Text genreText = new Text();
+
+        public void map(Object key, BSONObject value, Context context) 
+		throws IOException, InterruptedException {
+            String title = value.get("title").toString();
+            String genre = value.get("genres").toString().replace("\\|", ",");
+
+            if (title.startsWith("a")) {
+                titleText.set(title);
+                genreText.set(genre);
+                context.write(titleText, genreText);
+            }
         }
+    }
+
+    public static class Combine extends Reducer<Text, Text, Text, Text> {
+        private final Text combinerText = new Text();
+
+        public void reduce(Text key, Iterable<Text> values, Context context) 
+		throws IOException, InterruptedException {
+            CombinerText.set(values.iterator().next().toString());
+
+            context.write(key, CombinerText);
+        }
+    }
 	
-	public static class Reduce extends Reducer<Text, Text, Text, BSONWritable> {
-		private BSONWritable reduceResult = new BSONWritable();
+    public static class Reduce extends Reducer<Text, Text, Text, BSONWritable> {
+        private BSONWritable reduceResult = new BSONWritable();
 
-		public void reduce(Text key, Iterable<Text> values, Context context) 
-				throws IOException, InterruptedException {
+        public void reduce(Text key, Iterable<Text> values, Context context) 
+		throws IOException, InterruptedException {
+            BasicBSONObject output = new BasicBSONObject();
+            output.put("genre", values.iterator().next().toString());
 
-			BasicBSONObject output = new BasicBSONObject();
-			output.put("genre", values.iterator().next().toString());
-			reduceResult.setDoc(output);
-
-			context.write(key, reduceResult);
-		}
-	}
-
-	public static void main(String[] args) throws Exception {
-
-		final Configuration conf = new Configuration();
-		MongoConfigUtil.setInputURI(conf, "mongodb://" + args[0]);
-		MongoConfigUtil.setOutputURI(conf, "mongodb://" + args[1]);
-		System.out.println("Conf: " + conf);
-
-		final Job job = new Job(conf, "word count");
-
-		job.setJarByClass(WordCount.class);
-
-		job.setMapperClass(Map.class);
-
-		job.setCombinerClass(Combine.class);
-		job.setReducerClass(Reduce.class);
-
-		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(Text.class);
-
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(BSONWritable.class);
-
-		job.setInputFormatClass(MongoInputFormat.class);
-		job.setOutputFormatClass(MongoOutputFormat.class);
-
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
-	}
+            reduceResult.setDoc(output);
+            context.write(key, reduceResult);
+        }
+    }
 }
