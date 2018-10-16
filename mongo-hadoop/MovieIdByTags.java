d3e93d55d9c0494a7971cf311198e9ace8ea1d4b
Kyungyong Lee <leeky@kookmin.ac.kr>
2018-10-16 22:01:58 +0000

    interim working version

--- mongo-hadoop/MovieIdByTags.java
+++ mongo-hadoop/MovieIdByTags.java
@@ -17,83 +17,90 @@ import com.mongodb.hadoop.io.BSONWritable;
 import com.mongodb.hadoop.util.MongoConfigUtil;
 
 public class MovieIdByTags {
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        
+        MongoConfigUtil.setInputURI(conf, "mongodb://" + args[0]);
+        MongoConfigUtil.setOutputURI(conf, "mongodb://" + args[1]);
+        MongoConfigUtil.setQuery(conf, "{timestamp:{$gt : 1367757237}}");
 
-		Configuration conf = new Configuration();
-		MongoConfigUtil.setInputURI(conf, "mongodb://" + args[0]);
-		MongoConfigUtil.setOutputURI(conf, "mongodb://" + args[1]);
+        Job job = Job.getInstance(conf, "TagsAggregator");
 
-		Job job = new Job(conf, "word count");
+        job.setJarByClass(MovieIdByTags.class);
 
-		job.setJarByClass(MovieIdByTags.class);
+        job.setMapperClass(Map.class);
+        job.setCombinerClass(Combine.class);
+        job.setReducerClass(Reduce.class);
 
-		job.setMapperClass(Map.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
 
-		job.setCombinerClass(Combine.class);
-		job.setReducerClass(Reduce.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(BSONWritable.class);
 
-		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(Text.class);
+        job.setInputFormatClass(MongoInputFormat.class);
+        job.setOutputFormatClass(MongoOutputFormat.class);
 
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(BSONWritable.class);
+        job.waitForCompletion(true);
+    }
 
-		job.setInputFormatClass(MongoInputFormat.class);
-		job.setOutputFormatClass(MongoOutputFormat.class);
+    public static class Map extends Mapper<ObjectId, BSONObject, Text, Text> {
+        private final Text tagOutput = new Text();
+        private final Text movieIdOutput = new Text();
 
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
-	}
+        public void map(ObjectId key, BSONObject value, Context context) throws IOException, InterruptedException {
+            String tag = value.get("tag").toString();
+            String movieId = value.get("movieId").toString();
 
-	public static class Map extends Mapper<ObjectId, BSONObject, Text, Text> {
-		private final Text tagOutput = new Text();
-		private final Text movieIdOutput = new Text();
+            tagOutput.set(tag);
+            movieIdOutput.set(movieId);
+            context.write(tagOutput, movieIdOutput);
+        }
+    }
 
-		public void map(ObjectId key, BSONObject value, Context context) throws IOException, InterruptedException {
-			String tag = value.get("tag").toString();
-			String movieId = value.get("movieId").toString();
+    public static class Combine extends Reducer<Text, Text, Text, Text> {
+        private final Text combinerOutput = new Text();
 
-			tagOutput.set(tag);
-			movieIdOutput.set(movieId);
-			context.write(tagOutput, movieIdOutput);
-		}
-	}
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            HashSet<String> ids = new HashSet<>(); 
+            for (Text value : values) {
+                ids.add(value.toString());
+            }
 
-	public static class Combine extends Reducer<Text, Text, Text, Text> {
-		private final Text combinerOutput = new Text();
-		private StringBuilder sb = new StringBuilder();
-		private HashSet<String> set = new HashSet<>(); // 지역적으로라도 중복을 제거하기 위해 넣음.
+            Iterator itr = ids.iterator();
+            while (itr.hasNext()) {
+                combinerOutput.set(itr.next().toString());
+                context.write(key, combinerOutput);
+            }
+        }
+    }
 
-		public void reduce(Text key, Iterable<Text> values, Context context) 
-				throws IOException, InterruptedException {
-			for (Text value : values) {
-				set.add(value.toString());
-			}
-			Iterator itr = set.iterator();
-			while (itr.hasNext()) {
-				sb.append(itr.next().toString() + ", ");
-			}
-			combinerOutput.set(sb.toString());
-			context.write(key, combinerOutput);
-		}
-	}
+    public static class Reduce extends Reducer<Text, Text, Text, BSONWritable> {
+        private BSONWritable reduceResult = new BSONWritable();
 
-	public static class Reduce extends Reducer<Text, Text, Text, BSONWritable> {
-		private BSONWritable reduceResult = new BSONWritable();
-		private StringBuilder sb = new StringBuilder();
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            BasicBSONObject output = new BasicBSONObject();
+            HashSet<String> ids = new HashSet<>(); 
+            StringBuilder sb = new StringBuilder();
 
-		public void reduce(Text key, Iterable<Text> values, Context context) 
-				throws IOException, InterruptedException {
-			BasicBSONObject output = new BasicBSONObject();
-			for (Text value : values) {
-				sb.append(value.toString());
-			}
-			String movieIds = sb.toString();
-			if (movieIds.endsWith(", ")) {
-				movieIds = movieIds.substring(0, movieIds.length() - 2);
-			}
-			output.put("moiveIds", movieIds);
-			reduceResult.setDoc(output);
-			context.write(key, reduceResult);
-		}
-	}
+            for (Text value : values) {
+                ids.add(value.toString());
+            }
+            
+            Iterator itr = ids.iterator();
+            while (itr.hasNext()) {
+                sb.append(itr.next().toString());
+                sb.append(", ");
+            }
+
+            String movieIds = sb.toString();
+            if (movieIds.endsWith(", ")) {
+                movieIds = movieIds.substring(0, movieIds.length() - 2);
+            }
+            
+            output.put("moiveIds", movieIds);
+            reduceResult.setDoc(output);
+            context.write(key, reduceResult);
+        }
+    }
 }
